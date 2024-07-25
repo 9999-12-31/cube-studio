c4fe8baa7f5a7de3af92e7394b9dc5a43ad58a65
xiechenglong <xiechenglong@inspur.com>
2024-07-25 10:55:59 +0800

    vllm

--- images/serving/vllm/build.sh
+++ images/serving/vllm/build.sh
@@ -2,5 +2,5 @@
 
 set -ex
 
-docker build --network=host -t harbor.bigdata.com/cube-studio/vllm-server:20240724 -f Dockerfile .
-docker push harbor.bigdata.com/cube-studio/vllm-server:20240724
+docker build --network=host -t harbor.bigdata.com/cube-studio/vllm-server:20240725 -f Dockerfile .
+docker push harbor.bigdata.com/cube-studio/vllm-server:20240725
